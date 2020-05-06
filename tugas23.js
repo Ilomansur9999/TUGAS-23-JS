98a53c3ee429a1f2cb67f54884282d26f3d9dbd2
ilomansur9999 <ilofaqot@gmail.com>
2020-05-06 08:32:27 +0700

    TUGAS 23 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas23.js
@@ -0,0 +1,25 @@
+let angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+let angkabesar = angka.filter(item => item > 15);
+
+console.log(angkabesar);
+
+
+
+// C:\Users\USER\Documents\JS\javascript>node tugas23.js
+// [ 32, 33, 16, 40, 44, 66 ]
+
+
+
+
+
+
+
+
+// let angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+// let angkabesar = angka.filter(angka => angka > 15 {
+// 	return angka === "angkabesar";
+// });
+
+// console.log(angka);
